a0bce665e8d0812628a7bb23c49c7e03a6e68439
Shaam Aarya <99209007+shaamaarya1985@users.noreply.github.com>
2022-07-19 02:33:19 +0530

    Update container.php

--- container.php
+++ container.php
@@ -9,11 +9,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="http://www.phpzag.com" class="navbar-brand">PHPZAG.COM</a>
+          <a href="http://localhost/event-calender" class="navbar-brand">Event Calender</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="http://www.phpzag.com">Home</a></li>                      
+            <li class="active"><a href="http://localhost/event-calender">Home</a></li>                      
           </ul>
          <a href = "logout.php" style="">LogOut</a>
         </div><!--/.nav-collapse -->
